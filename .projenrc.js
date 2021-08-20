85ebc11f76e295aa91b179146ea16a18b4270c35
APrzysiuda <180517@umons.ac.be>
2021-08-20 14:11:53 +0200

    BREAKING CHANGE: initial commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, ProjectType } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'APrzysiuda',
   authorAddress: '180517@umons.ac.be',
@@ -6,7 +6,11 @@ const project = new AwsCdkConstructLibrary({
   defaultReleaseBranch: 'main',
   name: 'flywayProjenConstruct',
   repositoryUrl: 'https://github.com/180517/flywayProjenConstruct.git',
-
+  projectType: ProjectType.LIB,
+  cdkAssert: true,
+  cdkDependencies: ['@aws-cdk/core', '@aws-cdk/aws-lambda', '@aws-cdk/aws-ec2'],
+  docgen: true,
+  eslint: true,
   // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
   // deps: [],                          /* Runtime dependencies of this module. */
